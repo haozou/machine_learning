a767f5daf0a3b8a00c676d043d7e57f664b2040f
hazo <hazo@microsoft.com>
2019-11-05 13:39:03 -0800

    ad

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,8 @@
+home_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ ! -d ${home_dir}/.venv ]; then
+    echo "setting up virtual env"
+    virtualenv .venv
+fi
 source .venv/bin/activate
 cat requirements | awk '{system("pip install " $1);}'
 virtualenv --relocatable .venv
